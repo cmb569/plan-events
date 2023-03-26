6cb36cc7ba8a01e0d560fae997f37bab5e660c1d
Your Name <you@example.com>
2023-03-26 16:45:54 -0700

    Transfer js or jsx to ts or tsx

---
+++ Front-End/src/App.ts
@@ -0,0 +1,25 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+
+
+
+
+function App() {
+
+  return (
+    /////Homepage Information///////
+    <div className="App">
+      <header>
+      <h1 className="title">SPARKLY EVENT PLANNER</h1>
+      <NavBar />
+  
+  
+      </header>
+
+
+
+    </div>
+  );
+}
+
+export default App;
